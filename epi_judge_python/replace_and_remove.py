75476c843c1b8475e660ff6515316b8857d05881
Giuseppe Crinò <giuscri@gmail.com>
2019-04-07 18:16:23 +0200

    Solve "REPLACE AND REMOVE"
    Implement a cleaner solution after having read the solution from the book.

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,40 +5,38 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size, s):
-    many_as, many_bs = 0, 0
-    for i in range(size):
-        if s[i] == "b":
-            s[i] = ""
-            size -= 1
-            many_bs += 1
-        elif s[i] == "a":
-            many_as += 1
+    write_idx = 0
+    a_count = 0
+    b_count = 0
+    for c in s:
+        if c == "":
+            break
 
-    res_size = 2 * many_as + (size - many_as)
-    j = len(s) - 1
-    i = size + many_bs - 1
+        if c != "b":
+            s[write_idx] = c
+            write_idx += 1
+
+        if c == "a":
+            a_count += 1
+        elif c == "b":
+            b_count += 1
+
+
+    size -= b_count
+    res_size = 2 * a_count + (size - a_count)
+    j = res_size - 1
+    i = write_idx - 1
+
     while i >= 0:
-        if s[i] in ["c", "d"]:
+        if s[i] == "a":
+            s[j] = s[j - 1] = "d"
+            j -= 2
+        else:
             s[j] = s[i]
             j -= 1
-            i -= 1
-        elif s[i] == "a":
-            s[j] = "d"
-            s[j - 1] = "d"
-            j -= 2
-            i -= 1
-        elif s[i] == "":
-            i -= 1
-
-    j += 1
-    i = 0
-    while i < res_size:
-        s[i] = s[j]
-        i += 1
-        j += 1
+        i -= 1
 
     return res_size
-
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
@@ -47,7 +45,6 @@ def replace_and_remove_wrapper(executor, size, s):
 
 
 if __name__ == '__main__':
-    replace_and_remove(1, ["a", ""])
     exit(
         generic_test.generic_test_main("replace_and_remove.py",
                                        'replace_and_remove.tsv',

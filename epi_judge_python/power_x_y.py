f627ec31a017d4832ea92603ee9eb60fd904e84f
Giuseppe Crinò <giuscri@gmail.com>
2019-04-07 11:34:44 +0200

    Solve "COMPUTE xʸ"

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def power(x, y):
-    # TODO - you fill in here.
-    return 0.0
+    if y == 0:
+        return 1
+    if y == 1:
+        return x
+    elif y == -1:
+        return 1 / x
+
+    abs_y = abs(y)
+
+    p = power(x, abs_y >> 1)
+    r = None
+    if abs_y & 1 == 1:
+        r = p * p * x
+    else:
+        r = p * p
+
+    if y < 0:
+        return 1 / r
+    else:
+        return r
 
 
 if __name__ == '__main__':

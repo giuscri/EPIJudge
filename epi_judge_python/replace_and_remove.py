fe3d88141a98855824bd647d5ac6486a0940c684
Giuseppe Crinò <giuscri@gmail.com>
2019-04-07 18:03:00 +0200

    Solve "REPLACE AND REMOVE"
    Using a O(n) approach

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,23 +5,39 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size, s):
-    i = 0
-    while i < size:
-        if s[i] == "a":
-            for j in range(size - 1, i, -1):
-                s[j + 1] = s[j]
-            s[i] = "d"
-            s[i + 1] = "d"
-            size += 1
-            i += 2
-        elif s[i] == "b":
-            for j in range(i + 1, size):
-                s[j - 1] = s[j]
+    many_as, many_bs = 0, 0
+    for i in range(size):
+        if s[i] == "b":
+            s[i] = ""
             size -= 1
-        else:
-            i += 1
+            many_bs += 1
+        elif s[i] == "a":
+            many_as += 1
 
-    return size
+    res_size = 2 * many_as + (size - many_as)
+    j = len(s) - 1
+    i = size + many_bs - 1
+    while i >= 0:
+        if s[i] in ["c", "d"]:
+            s[j] = s[i]
+            j -= 1
+            i -= 1
+        elif s[i] == "a":
+            s[j] = "d"
+            s[j - 1] = "d"
+            j -= 2
+            i -= 1
+        elif s[i] == "":
+            i -= 1
+
+    j += 1
+    i = 0
+    while i < res_size:
+        s[i] = s[j]
+        i += 1
+        j += 1
+
+    return res_size
 
 
 @enable_executor_hook
@@ -31,6 +47,7 @@ def replace_and_remove_wrapper(executor, size, s):
 
 
 if __name__ == '__main__':
+    replace_and_remove(1, ["a", ""])
     exit(
         generic_test.generic_test_main("replace_and_remove.py",
                                        'replace_and_remove.tsv',

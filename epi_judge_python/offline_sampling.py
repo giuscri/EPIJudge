476f3d6627ff638304b1e52d90ef2031eae22341
Giuseppe Crinò <giuscri@gmail.com>
2019-04-15 20:57:35 +0200

    Solve "SAMPLE OFFLINE DATA"

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -6,10 +6,13 @@ from test_framework.random_sequence_checker import (
     compute_combination_idx, run_func_with_retries)
 from test_framework.test_utils import enable_executor_hook
 
+from random import randint
 
 def random_sampling(k, A):
-    # TODO - you fill in here.
-    return
+    for i in range(k):
+        j = randint(i, len(A) - 1)
+        A[i], A[j] = A[j], A[i]
+    return A
 
 
 @enable_executor_hook

bf578c3cad671c658cb47c8e652274f2bed6b933
Giuseppe Crinò <giuscri@gmail.com>
2019-04-15 22:08:53 +0200

    Solve "COMPUTE THE SPIRAL ORDERING OF A 2D ARRAY"

--- epi_judge_python/spiral_ordering_segments.py
+++ epi_judge_python/spiral_ordering_segments.py
@@ -2,8 +2,40 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix):
-    # TODO - you fill in here.
-    return []
+    if not square_matrix:
+        return []
+
+    N = len(square_matrix)
+    padding = 0
+    r = []
+    while N - 2*padding > 1:
+        x = y = padding
+
+        for _ in range(N - 2*padding):
+            r.append(square_matrix[x][y])
+            y += 1
+        x += 1
+        y -= 1
+        for _ in range(N - 2*padding - 1):
+            r.append(square_matrix[x][y])
+            x += 1
+        x -= 1
+        y -= 1
+        for _ in range(N - 2*padding - 1):
+            r.append(square_matrix[x][y])
+            y -= 1
+        y += 1
+        x -= 1
+        for _ in range(N - 2*padding - 2):
+            r.append(square_matrix[x][y])
+            x -= 1
+
+        padding += 1
+
+    if N - 2*padding == 1:
+        r.append(square_matrix[padding][padding])
+
+    return r
 
 
 if __name__ == '__main__':

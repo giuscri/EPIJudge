233ef664ab1f6de09c6e4938b58cbd850ccfb295
Giuseppe Crinò <giuscri@gmail.com>
2019-04-18 23:21:59 +0200

    Solve "COMPUTE THE LCA WHEN NODES HAVE PARENT POINTERS"
    , after having read some hints from the book!

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -5,21 +5,40 @@ from test_framework.binary_tree_utils import must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+def depth(node):
+    """Compute the depth for a node of a binary tree."""
+    d = 0
+    iterator = node.parent
+    while iterator:
+        d += 1
+        iterator = iterator.parent
+    return d
 
 def lca(node0, node1):
-    node0_ancestors = set()
-    ancestor = node0
-    while ancestor:
-        node0_ancestors.add(ancestor)
-        ancestor = ancestor.parent
+    """Compute the lowest common ancestor of node0 and node1."""
+    node0_depth = depth(node0)
+    node1_depth = depth(node1)
 
-    ancestor = node1
-    while ancestor:
-        if ancestor in node0_ancestors:
-            return ancestor
-        ancestor = ancestor.parent
+    node0_ancestor = node0
+    node1_ancestor = node1
 
-    raise ValueError(f"{node0} and {node1} do not belong to the same tree.")
+    if node0_depth > node1_depth:
+        while node0_depth > node1_depth:
+            node0_ancestor = node0_ancestor.parent
+            node0_depth -= 1
+    elif node1_depth > node0_depth:
+        while node1_depth > node0_depth:
+            node1_ancestor = node1_ancestor.parent
+            node1_depth -= 1
+
+    while node0_ancestor is not node1_ancestor:
+        if node0_ancestor is None or node1_ancestor is None:
+            raise ValueError(f"{node0} and {node1} do not belong to the same tree.")
+
+        node0_ancestor = node0_ancestor.parent
+        node1_ancestor = node1_ancestor.parent
+
+    return node0_ancestor
 
 
 @enable_executor_hook

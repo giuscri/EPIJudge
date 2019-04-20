d0b544279331cc2aa361c9c232a717222274cc60
Giuseppe Crinò <giuscri@gmail.com>
2019-04-20 12:50:08 +0200

    Solve "SEARCH A SORTED ARRAY FOR FIRST OCCURRENCE OF K"

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
-
+from math import inf
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    m = inf
+    start, stop = 0, len(A) - 1
+    while start <= stop:
+        idx = start + (stop - start) // 2
+        if A[idx] == k:
+            m = min(m, idx)
+            start, stop = start, idx - 1
+        elif A[idx] > k:
+            start, stop = start, idx - 1
+        else:
+            start, stop = idx + 1, stop
+    return -1 if m is inf else m
 
 
 if __name__ == '__main__':

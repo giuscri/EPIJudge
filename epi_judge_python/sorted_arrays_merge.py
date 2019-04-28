b98f27cfdb4562915847f8f9370924cda0ce6c6a
Giuseppe Crinò <giuscri@gmail.com>
2019-04-28 15:42:56 +0200

    Solve "MERGE SORTED FILES"
    Using a different approach, moving from O(n * log(n)) to O(n * log(k)) where k
    is the number of sorted arrays.

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,75 +1,94 @@
 from test_framework import generic_test
 
-def maximum(max_heap):
-    """Returns maximum value stored in max_heap."""
+class HeapNode:
+    """A node in the heap.
 
-    if max_heap is None or max_heap == []:
+    It stores an integer, the index of the array the integer is coming from,
+    and the index of the integer in that array."""
+
+    def __init__(self, value, from_array_idx, in_array_idx):
+        self.value = value
+        self.from_array_idx = from_array_idx
+        self.in_array_idx = in_array_idx
+
+    def __repr__(self):
+        return f"({self.value}, {self.from_array_idx}, {self.in_array_idx})"
+
+def minimum(min_heap):
+    """Returns minimum value stored in min_heap."""
+
+    if min_heap is None or min_heap == []:
         return None
 
-    return max_heap[0]
+    return min_heap[0]
 
-def insert(max_heap, x):
-    """Inserts `x` into provided max_heap."""
-    if max_heap is None:
-        max_heap = []
+def insert(min_heap, n):
+    """Inserts node `n` into provided min_heap."""
 
-    max_heap.append(x)
-    child_idx = len(max_heap) - 1
+    if min_heap is None:
+        min_heap = []
+
+    min_heap.append(n)
+    child_idx = len(min_heap) - 1
     while child_idx >= 0:
         parent_idx = (child_idx - 1) // 2
 
-        if parent_idx >= 0 and max_heap[child_idx] > max_heap[parent_idx]:
-            max_heap[child_idx], max_heap[parent_idx] = max_heap[parent_idx], max_heap[child_idx]
+        if parent_idx >= 0 and min_heap[child_idx].value < min_heap[parent_idx].value:
+            min_heap[child_idx], min_heap[parent_idx] = min_heap[parent_idx], min_heap[child_idx]
         else:
             break # we don't need to check this further
 
         child_idx = parent_idx
 
-    return max_heap
+    return min_heap
 
-def extract_maximum(max_heap):
-    """Pops out maximum from max_heap, returns max_heap with its properties restored."""
+def extract_minimum(min_heap):
+    """Pops out mininum from min_heap, returns min_heap with its properties restored."""
 
-    if max_heap is None or max_heap == []:
-        return None, max_heap
+    if min_heap is None or min_heap == []:
+        return None, min_heap
 
-    max_heap[0], max_heap[-1] = max_heap[-1], max_heap[0]
-    previous_maximum = max_heap[-1]
-    max_heap = max_heap[:-1] # slicing off the root just moved to the end of the array
+    min_heap[0], min_heap[-1] = min_heap[-1], min_heap[0]
+    previous_minimum = min_heap[-1]
+    min_heap = min_heap[:-1] # slicing off the root just moved to the end of the array
     parent_idx = 0
-    while parent_idx < len(max_heap):
+    while parent_idx < len(min_heap):
         left_child_idx = 2 * parent_idx + 1
         right_child_idx = 2 * parent_idx + 2
 
-        if left_child_idx >= len(max_heap):
+        if left_child_idx >= len(min_heap):
             break
 
-        if right_child_idx < len(max_heap) and max_heap[right_child_idx] > max_heap[left_child_idx]:
+        if right_child_idx < len(min_heap) and min_heap[right_child_idx].value < min_heap[left_child_idx].value:
             child_idx = right_child_idx
         else:
             child_idx = left_child_idx
 
-        if max_heap[child_idx] > max_heap[parent_idx]:
-            max_heap[child_idx], max_heap[parent_idx] = max_heap[parent_idx], max_heap[child_idx]
+        if min_heap[child_idx].value < min_heap[parent_idx].value:
+            min_heap[child_idx], min_heap[parent_idx] = min_heap[parent_idx], min_heap[child_idx]
             parent_idx = child_idx
         else:
             break # we don't need to check this further
 
-    return previous_maximum, max_heap
+    return previous_minimum, min_heap
 
 def merge_sorted_arrays(sorted_arrays):
+    min_heap = None
+    for i, array in enumerate(sorted_arrays):
+        if not array:
+            continue
+
+        min_heap = insert(min_heap, HeapNode(array[0], i, 0))
+
     r = []
-    max_heap = None
-    for array in sorted_arrays:
-        for x in array:
-            max_heap = insert(max_heap, x)
+    while minimum(min_heap) is not None: # minimum() returns None when the heap is empty
+        m, min_heap = extract_minimum(min_heap)
+        r.append(m.value)
+        if m.in_array_idx + 1 < len(sorted_arrays[m.from_array_idx]):
+            n = HeapNode(sorted_arrays[m.from_array_idx][m.in_array_idx + 1], m.from_array_idx, m.in_array_idx + 1)
+            min_heap = insert(min_heap, n)
 
-    x, max_heap = extract_maximum(max_heap)
-    while x is not None:
-        r.append(x)
-        x, max_heap = extract_maximum(max_heap)
-
-    return list(reversed(r))
+    return r
 
 if __name__ == '__main__':
     exit(

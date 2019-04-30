b11b116425e1a504dfdbc9f8fa18dd0d2bce87a5
Giuseppe Crinò <giuscri@gmail.com>
2019-04-30 08:57:07 +0200

    Solve "PARTITION INTO ANAGRAMS"

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -2,8 +2,19 @@ from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary):
-    # TODO - you fill in here.
-    return []
+    anagrams = dict()
+    for word in dictionary:
+        #key = "".join(sorted(filter(lambda character: character != " ", word)))
+        key = "".join(sorted("".join(word.split(" ")))) # way more efficient!
+        value = anagrams.get(key, [])
+        value.append(word)
+        anagrams[key] = value
+
+    r = []
+    for k in anagrams.keys():
+        if len(anagrams.get(k, [])) > 1:
+            r.append(anagrams[k])
+    return r
 
 
 if __name__ == '__main__':

517ac880a27051af41e5d547fa2ae3fcdacc30e9
Giuseppe Crinò <giuscri@gmail.com>
2019-05-01 17:06:36 +0200

    Solve "IMPLEMENT MERGE SORT IN-PLACE"

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -2,11 +2,37 @@ from test_framework import generic_test
 
 
 def merge_two_sorted_arrays(A, m, B, n):
-    for i in range(len(B)):
-        A[m + i] = B[i]
-    A.sort()
-    return A
+    """Merges A and B, where A has enough entries to store them both.
 
+    The idea is the keep three indices: i, j and k. i points to the next item
+    of A to process, j to the next item of B to process and k points to the
+    next entry of A that we can overwrite.
+
+    The procedure runs in O(1) memory and it has a time complexity of O(|A|).
+    """
+
+    i, j, k = m - 1, n - 1, len(A) - 1
+    while k >= 0:
+        if i < 0:
+            assert j >= 0
+            A[k] = B[j]
+            k -= 1
+            j -= 1
+        elif j < 0:
+            assert i >=0
+            A[k] = A[i]
+            k -= 1
+            i -= 1
+        elif A[i] > B[j]:
+            A[k] = A[i]
+            i -= 1
+            k -= 1
+        else:
+            A[k] = B[j]
+            j -= 1
+            k -= 1
+
+    return A
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
     merge_two_sorted_arrays(A, m, B, n)

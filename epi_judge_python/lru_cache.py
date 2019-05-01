abd623df356818743a65730d1dbe3fe6560ebf12
Giuseppe Crinò <giuscri@gmail.com>
2019-05-01 15:57:21 +0200

    Solve "IMPLEMENT AN ISBN CACHE"

--- epi_judge_python/lru_cache.py
+++ epi_judge_python/lru_cache.py
@@ -1,22 +1,78 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from math import inf
 
 class LruCache:
+    """Implements LRU cache of book prices.
+
+    The LRU policy is implemented by storing, together with the book price,
+    the _rank_. The rank is incremented throughout the life of the cache.
+    It's like a ticking clock: each time a new element enters the cache the
+    rank is incremented. Hence, the element with the lowest rank is the
+    element that's the oldest.
+    """
+
     def __init__(self, capacity):
-        # TODO - you fill in here.
+        self.capacity = capacity
+        self.n = 0
+        self.current_rank = 0
+        self.dict = dict()
         return
 
     def lookup(self, isbn):
-        # TODO - you fill in here.
-        return 0
+        """Lookups price book. Returns -1 if there's no book for that ISBN."""
+
+        price, rank = self.dict.get(isbn, (-1, None))
+        if price >= 0: # book is in cache
+            self.dict[isbn] = (price, self.current_rank)
+
+            # Prepare current_rank for the next assignment
+            self.current_rank += 1
+
+        return price
 
     def insert(self, isbn, price):
-        # TODO - you fill in here.
+        """Inserts price for `isbn`, removes LRU element if exceeding capacity."""
+
+        if isbn in self.dict.keys():
+            # Just update the rank of the cache entry
+
+            price, rank = self.dict[isbn]
+            self.dict[isbn] = (price, self.current_rank)
+
+            # Prepare `current_rank` for the next assignment
+            self.current_rank += 1
+
+            return # we don't want to replace values already in the cache
+
+        if self.n >= self.capacity:
+            lowest_rank = inf
+            isbn_with_lowest_rank = None
+            for key in self.dict:
+                _, rank = self.dict[key]
+                if rank < lowest_rank:
+                    lowest_rank = rank
+                    isbn_with_lowest_rank = key
+
+            self.erase(isbn_with_lowest_rank)
+
+        self.dict[isbn] = (price, self.current_rank)
+
+        # Increment the number of keys stored in the cache
+        self.n += 1
+
+        # Increment rank for the next insertion
+        self.current_rank += 1
+
         return
 
     def erase(self, isbn):
-        # TODO - you fill in here.
+        if isbn not in self.dict:
+            return False
+
+        del self.dict[isbn] # https://stackoverflow.com/questions/51800639/complexity-of-deleting-a-key-from-python-ordered-dict
+        self.n -= 1
         return True
 
 

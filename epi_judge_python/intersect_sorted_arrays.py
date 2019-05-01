22b355cbfd5a7df8026e0a78d2267d14245768dc
Giuseppe Crinò <giuscri@gmail.com>
2019-05-01 17:06:04 +0200

    Solve "COMPUTE THE INTERSECTION OF TWO SORTED ARRAYS"

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    intersection = []
+    i, j = 0, 0
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if intersection == [] or A[i] != intersection[-1]:
+                # don't want to insert duplicates in `intersection`
+                intersection.append(A[i])
+
+            i += 1
+            j += 1
+        elif A[i] < B[j]:
+            i += 1
+        elif A[i] > B[j]:
+            j += 1
+    return intersection
 
 
 if __name__ == '__main__':

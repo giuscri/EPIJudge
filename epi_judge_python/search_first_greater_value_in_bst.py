1b445cf78c91d993e3bfab7665334892d34a2a47
Giuseppe Crinò <giuscri@gmail.com>
2019-05-02 22:40:14 +0200

    Solve "FIRST THE FIRST KEY LARGER THAN A GIVEN VALUE IN A BST"

--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -1,10 +1,30 @@
 from test_framework import generic_test
 
+def _find_first_greater_than_k(tree, k):
+    if tree is None:
+        return []
+
+    sorted_keys = []
+    sorted_keys.extend(_find_first_greater_than_k(tree.left, k))
+    sorted_keys.append(tree.data)
+    sorted_keys.extend(_find_first_greater_than_k(tree.right, k))
+
+    return sorted_keys
 
 def find_first_greater_than_k(tree, k):
-    # TODO - you fill in here.
-    return None
+    class R:
+        def __init__(self, data):
+            self.data = data
 
+    sorted_keys = _find_first_greater_than_k(tree, k)
+    previous = None
+    for key in sorted_keys:
+        if (previous is None or previous <= k) and key > k:
+            return R(key)
+
+        previous = key
+
+    return None
 
 def find_first_greater_than_k_wrapper(tree, k):
     result = find_first_greater_than_k(tree, k)

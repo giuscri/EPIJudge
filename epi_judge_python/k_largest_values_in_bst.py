f52e5bc39afb32e923130f552e029176cbe349ff
Giuseppe Crinò <giuscri@gmail.com>
2019-05-03 08:27:14 +0200

    Solve "FIND THE K LARGEST ELEMENTS IN A BST"
    This approach is still O(n), but it should be O(k) if we map the recursions to
    iterations!

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -1,42 +1,28 @@
 from test_framework import generic_test, test_utils
 
-def count_nodes_in_bst(tree):
-    if tree is None:
-        return 0
-
-    n = 1
-    if tree.left is not None:
-        n += count_nodes_in_bst(tree.left)
-    if tree.right is not None:
-        n += count_nodes_in_bst(tree.right)
-
-    return n
-
-def in_order_traversal(tree):
-    if tree is None:
-        return []
-
-    sorted_keys = []
-
-    if tree.left is not None:
-        sorted_keys.extend(in_order_traversal(tree.left))
-
-    sorted_keys.append(tree.data)
+def _find_k_largest_in_bst(tree, k, r):
+    if k == 0:
+        return r, k
 
     if tree.right is not None:
-        sorted_keys.extend(in_order_traversal(tree.right))
+        r, k = _find_k_largest_in_bst(tree.right, k, r)
+    if k == 0:
+        return r, k
 
-    return sorted_keys
+    r.append(tree.data)
+    k -= 1
+    if k == 0:
+        return r, k
+
+    if tree.left is not None:
+        r, k = _find_k_largest_in_bst(tree.left, k, r)
+
+    return r, k
 
 def find_k_largest_in_bst(tree, k):
-    n = count_nodes_in_bst(tree)
-    sorted_keys = in_order_traversal(tree)
-    k_largest = []
-    for i in range(n - k, n):
-        k_largest.append(sorted_keys[i])
-
-    return k_largest
-
+    r, k = _find_k_largest_in_bst(tree, k, [])
+    assert k == 0
+    return r
 
 if __name__ == '__main__':
     exit(

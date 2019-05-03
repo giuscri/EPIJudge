213eb60d98bf18602c088f00d7346b79aaaff4e8
Giuseppe Crinò <giuscri@gmail.com>
2019-05-03 08:27:14 +0200

    Solve "FIND THE K LARGEST ELEMENTS IN A BST"
    Remove recursion. This approach has a time complexity of O(k).

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -1,28 +1,32 @@
 from test_framework import generic_test, test_utils
 
-def _find_k_largest_in_bst(tree, k, r):
-    if k == 0:
-        return r, k
-
-    if tree.right is not None:
-        r, k = _find_k_largest_in_bst(tree.right, k, r)
-    if k == 0:
-        return r, k
-
-    r.append(tree.data)
-    k -= 1
-    if k == 0:
-        return r, k
-
-    if tree.left is not None:
-        r, k = _find_k_largest_in_bst(tree.left, k, r)
-
-    return r, k
-
 def find_k_largest_in_bst(tree, k):
-    r, k = _find_k_largest_in_bst(tree, k, [])
-    assert k == 0
-    return r
+    if tree is None:
+        return []
+
+    result = []
+    stack = [tree]
+    pushed = set()
+
+    while len(result) < k and len(stack) > 0:
+        node = stack.pop()
+        if node.left is None and node.right is None:
+            result.append(node.data)
+        elif (node.left is not None and node.left in pushed) or (node.right is not None and node.right in pushed):
+            result.append(node.data)
+        else:
+            if node.left is not None:
+                stack.append(node.left)
+                pushed.add(node.left)
+
+            stack.append(node)
+            pushed.add(node)
+
+            if node.right is not None:
+                stack.append(node.right)
+                pushed.add(node.right)
+
+    return result
 
 if __name__ == '__main__':
     exit(

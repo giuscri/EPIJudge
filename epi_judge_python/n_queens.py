e7311fee9e2f738e0575d2dc8f3fe71f05fc02d8
Giuseppe Crinò <giuscri@gmail.com>
2019-05-04 22:48:44 +0200

    Solve "GENERATE ALL NONATTACKING PLACEMENTS OF N-QUEENS"

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -1,10 +1,31 @@
 from test_framework import generic_test
 
+def non_attacking_placements(board):
+    if len(board) < 1:
+        return [[]]
+
+    placements = []
+
+    first_board_row = board[0]
+    for i, square in enumerate(first_board_row):
+        if square == 1: # square is disabled
+            continue
+
+        copied_board = [row[:] for row in board]
+        for j, row in enumerate(copied_board):
+            row[i] = 1
+            if i - j >= 0:
+                row[i - j] = 1
+            if i + j < len(row):
+                row[i + j] = 1
+
+        placements.extend(map(lambda placement: [i] + placement, non_attacking_placements(copied_board[1:])))
+
+    return placements
 
 def n_queens(n):
-    # TODO - you fill in here.
-    return []
-
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    return non_attacking_placements(board)
 
 def comp(a, b):
     return sorted(a) == sorted(b)

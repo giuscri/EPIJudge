ee377c1fc98842135f16dfe808ff59c72ae28df6
Giuseppe Crinò <giuscri@gmail.com>
2019-05-05 22:16:37 +0200

    [wip] Solve "COUNT THE NUMBER OF SCORE COMBINATIONS"

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,10 +1,17 @@
 from test_framework import generic_test
 
+from functools import reduce
+from itertools import product
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
-    # TODO - you fill in here.
-    return 0
+    upper_bounds = map(lambda s, final_score=final_score: final_score // s, individual_play_scores)
+    iterables = tuple(map(lambda b: tuple(range(b + 1)), upper_bounds))
 
+    n = 0
+    for coefficients in product(*iterables):
+        if reduce(lambda a, p: a + p[0] * p[1], zip(coefficients, individual_play_scores), 0) == final_score:
+            n += 1
+    return n
 
 if __name__ == '__main__':
     exit(

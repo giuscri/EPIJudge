6105ae4b907571ccd47fefbc043b38bb76a8d353
Giuseppe Crinò <giuscri@gmail.com>
2019-05-16 08:48:27 +0200

    BONUS POINT: Solve "IMPLEMENT SYNCHRONIZATION FOR TWO INTERLEAVING THREADS"
    Avoid busy waiting anti-pattern.

--- epi_judge_python/interleaving_threads.py
+++ epi_judge_python/interleaving_threads.py
@@ -1,32 +1,41 @@
-from threading import Thread, RLock, get_ident
+from threading import Thread, RLock, get_ident, Condition
 from time import sleep
 from random import randint
 
 import sys
 
-LOCK = RLock()
-COUNTER = 0
+ODD, EVEN = 0, 1
+CURRENT_TURN = EVEN
+CONDITION = Condition()
+N = 100
 
-def print_and_increase(only_odd=True):
-    global COUNTER
+def print_odd_numbers():
+    for i in range(1, N + 1, 2):
+        wait_turn(ODD)
+        print(i)
+        toggle_turn()
 
-    i, only_even = 0, not only_odd
+def print_even_numbers():
+    for i in range(0, N + 1, 2):
+        wait_turn(EVEN)
+        print(i)
+        toggle_turn()
 
-    while i < 100:
-        LOCK.acquire()
+def wait_turn(turn):
+    if turn != CURRENT_TURN:
+        CONDITION.acquire()
+        CONDITION.wait() # wait() releases the lock and reacquire it when it wakes up
+        CONDITION.release()
 
-        if COUNTER % 2 == 0 and only_even:
-            COUNTER += 1
-            i += 1
-            print(f"{COUNTER} - printed by thread {get_ident()}")
-        elif COUNTER % 2 != 0 and only_odd:
-            COUNTER += 1
-            i += 1
-            print(f"{COUNTER} - printed by thread {get_ident()}")
+def toggle_turn():
+    global CURRENT_TURN
+    CURRENT_TURN = (CURRENT_TURN + 1) % 2
 
-        LOCK.release()
+    CONDITION.acquire()
+    CONDITION.notify()
+    CONDITION.release()
 
-t1, t2 = Thread(target=print_and_increase, kwargs={"only_odd": True}), Thread(target=print_and_increase, kwargs={"only_odd": False})
+t1, t2 = Thread(target=print_even_numbers), Thread(target=print_odd_numbers)
 
 for thread in [t1, t2]:
     thread.start()
